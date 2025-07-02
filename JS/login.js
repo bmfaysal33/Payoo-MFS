6e8dec8db01f3524ee0fff7faeae1cec8be3d316
BM Faysal <bmfaysal33@gmail.com>
2025-07-02 23:58:03 +0600

    added prevent default and event handler

--- JS/login.js
+++ JS/login.js
@@ -4,6 +4,21 @@
 
 
 
-     document.getElementById('button-login').addEventListener('click', function(){
+     document.getElementById('button-login').addEventListener('click', function(event){
+        event.preventDefault(); // This one prevents website from reloading after Text or Pin input
         console.log('Login button clicked...')
+
+
+                // step 3: Get the phone number
+
+                const phoneNumber = document.getElementById('phone-number').value;
+                        // phoneNumber.value= '01741957554';
+
+                        console.log(phoneNumber)
+
+
+
+
      })
+
+
