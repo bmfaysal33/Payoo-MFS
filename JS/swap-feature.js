4b5de5bc8a4cc5ac669dc6f4a7154529ecc0469c
BM Faysal <bmfaysal33@gmail.com>
2025-07-06 15:57:31 +0600

    Add money and Cash out button feature done, now heading towards UI and UX of these button

--- JS/swap-feature.js
+++ JS/swap-feature.js
@@ -1,3 +1,32 @@
 
-console.log('Function works...')
+
+        // Add Money Form....
+
+document.getElementById('btn-add-money-form').addEventListener('click', function(){
+
+    // Show cash out form....
+    document.getElementById('add-money-form').classList.remove('hidden');
+
+    // Hide add money form     
+
+    document.getElementById('cash-out-form').classList.add('hidden');
+
+})
+
+
+
+                        // Cash Out Form....                  
+
+document.getElementById('btn-cash-out-form').addEventListener('click', function(){
+
+    // Show cash out form....
+    document.getElementById('cash-out-form').classList.remove('hidden');
+
+    // Hide add money form
+
+    document.getElementById('add-money-form').classList.add('hidden');
+
+})
+
+
 
